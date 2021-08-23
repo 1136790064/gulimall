2baef88d2a4aca7826b06c64243f702d2f9537cd
zhuguangdeyingzi <1136790064@qq.com>
2021-08-23 18:00:42 +0800

    项目初始结构创建

---
+++ gulimall-order/src/test/java/com/zhuguangdeyingzi/gulimail/gulimailorder/GulimailOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuguangdeyingzi.gulimail.gulimailorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
